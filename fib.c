78de127a8ed0aa217f5b44d5415ab84939a044e8
Philip <huakeng@csu.fullerton.edu>
2024-09-05 10:39:59 -0700

    first commit

--- fib.c
+++ fib.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h> // atoi() function
 
-int main() {
-   printf("Hello!\n");
+unsigned long fibonacci_recursive(int num)
+{
+   if (num <= 1)
+      return num;
+   return fibonacci_recursive(num - 1) + fibonacci_recursive(num - 2);
+}
+
+unsigned long fibonacci_iterative(int num)
+{
+   if (num <= 1)
+   {
+      return num;
+   }
+
+   int first_fibonacci_num = 0, second_fibonacci_num = 1, next_fibonacci_num;
+
+   for (int ix = 2; ix <= num; ix++)
+   {
+      next_fibonacci_num = first_fibonacci_num + second_fibonacci_num;
+      first_fibonacci_num = second_fibonacci_num;
+      second_fibonacci_num = next_fibonacci_num;
+   }
+
+   return second_fibonacci_num;
+}
+
+int main(int argc, char *argcv[])
+{
+   int command_line_integer = atoi(argcv[1]);
+   char command_line_letter = argcv[2][0];
+   int integer_in_file;
+
+   FILE *file_pointer = fopen(argcv[3], "r");
+   fscanf(file_pointer, "%d", &integer_in_file);
+   fclose(file_pointer);
+
+   // Subtract 1 as the first number in fibonacci sequence is 0
+   int num_to_fibonacci = command_line_integer + integer_in_file - 1;
+   unsigned long fibonacci_result;
+
+   if (command_line_letter == 'r')
+   {
+      fibonacci_result = fibonacci_recursive(num_to_fibonacci);
+   }
+   if (command_line_letter == 'i')
+   {
+      fibonacci_result = fibonacci_iterative(num_to_fibonacci);
+   }
+
+   printf("%lu", fibonacci_result);
    return 0;
 }
