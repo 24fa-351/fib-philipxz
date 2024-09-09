6d61d9e26b444a4603ead0f143084cacdd6aa36e
Philip Xie Zhen <huakeng@csu.fullerton.edu>
2024-09-08 19:03:27 -0700

    done

--- fib.c
+++ fib.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h> // atoi() function
 
-unsigned long fibonacci_recursive(int num)
+unsigned long long fibonacci_recursive(int num)
 {
    if (num <= 1)
       return num;
    return fibonacci_recursive(num - 1) + fibonacci_recursive(num - 2);
 }
 
-unsigned long fibonacci_iterative(int num)
+unsigned long long fibonacci_iterative(int num)
 {
    if (num <= 1)
    {
       return num;
    }
 
-   int first_fibonacci_num = 0, second_fibonacci_num = 1, next_fibonacci_num;
+   unsigned long long first_fibonacci_num = 0, second_fibonacci_num = 1, next_fibonacci_num;
 
    for (int ix = 2; ix <= num; ix++)
    {
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 
    // Subtract 1 as the first number in fibonacci sequence is 0
    int num_to_fibonacci = command_line_integer + integer_in_file - 1;
-   unsigned long fibonacci_result;
+   unsigned long long fibonacci_result;
 
    if (command_line_letter == 'r')
    {
@@ -50,6 +50,6 @@ int main(int argc, char *argv[])
       fibonacci_result = fibonacci_iterative(num_to_fibonacci);
    }
 
-   printf("%lu", fibonacci_result);
+   printf("%llu\n", fibonacci_result);
    return 0;
 }
